f61c002bdbdec7eb6d3ba30114e24004a3a57762
wstrend <qisthi3407@students.amikom.ac.id>
2016-06-21 21:23:18 +0700

    Rancangan halaman dasbor member

--- app/views/member/index.php
+++ app/views/member/index.php
@@ -57,7 +57,7 @@
 
 				<ul class="sidebar-menu">
 					<li class="header">Main Navigation</li>
-					<li class="active"><a href="#"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+					<li class="active"><a href="<?= base_url('member') ?>"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
 					<li><a href="#"><i class="fa fa-car"></i><span>Bengkel</span></a></li>
 				</ul>
 			</section>
@@ -69,7 +69,53 @@
 			</section>
 
 			<section class="content">
-				<p>Ini konten</p>
+				<div class="row">
+					<div class="col-md-2">
+						<div class="box box-success">
+							<div class="box-header with-border">
+								<h3 class="box-title">Data Member</h3>
+							</div>
+							<div class="box-body">
+								<i class="fa fa-home"></i> 
+								<strong>Nama</strong>
+								<p>Qisthi Ramadhani</p>
+								<i class="fa fa-envelope"></i> 
+								<strong>Email</strong>
+								<p>qisthi@ramadhani.me</p>
+								<i class="fa fa-newspaper-o"></i> 
+								<strong>Keterangan</strong>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+								<i class="fa fa-paper-plane"></i> 
+								<strong>Terdaftar</strong>
+								<p>Kamis, 12 Mei 2016 pukul 14:20:24</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-10">
+						<div class="box box-success">
+							<div class="box-header">
+								<h3 class="box-title">Daftar Bengkel</h3>
+							</div>
+							<div class="box-body no-padding">
+								<table class="table table-condensed">
+									<tr>
+										<th>Nama</th>
+										<th style="width:30%">Alamat</th>
+										<th>Telepon</th>
+										<th style="width:30%">Keterangan</th>
+									</tr>
+									<tr>
+										<td>Bengkel Rama</td>
+										<td>Desa Setren RT 25/09, Kec. Bendo, Kab. Magetan</td>
+										<td>+6285785773484</td>
+										<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
+										<td><a href="#" class="btn btn-info" role="button">Edit</a></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 
